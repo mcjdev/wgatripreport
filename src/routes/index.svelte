a421771b67ca203b191fbca003508921d49c9377
Michael Johnson <aug484@me.com>
2022-02-20 15:29:20 +0000

    global css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,10 @@
 <script>
     import FlightCard from "$lib/flightCard.svelte";
     import FlightList from "$lib/flightList.svelte";
-    import Position from "$lib/position.svelte";
 </script>
 
 <svelte:head>
     <title>Welcome</title>
-    <style>
-        @font-face {
-            font-family: "Work Sans";
-            src: url("/fonts/WorkSans-VariableFont_wght.ttf");
-        }
-    </style>
 </svelte:head>
 
 <main>
